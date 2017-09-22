b2a8315b20dac7ff6b568c6958ac101801c6d9ad
Andreychenko <andreychenko.original@gmail.com>
2017-09-22 10:36:59 +0300

    Removed unused images

--- app/src/main/java/com/braincollaboration/wquote/widget/AlphaAnimationImageView.java
+++ app/src/main/java/com/braincollaboration/wquote/widget/AlphaAnimationImageView.java
@@ -1,10 +1,8 @@
 package com.braincollaboration.wquote.widget;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.DrawableRes;
 import android.util.AttributeSet;
-import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 
 import com.braincollaboration.wquote.R;
@@ -20,8 +18,8 @@ public class AlphaAnimationImageView extends android.support.v7.widget.AppCompat
     private final static long ANIM_DURATION = 750;
     private BackgroundImageSource imageSrc;
     private int pare;
-    private final static int[] backgroundSrc1 = {R.drawable.b11, R.drawable.b21, R.drawable.b31, R.drawable.b41, R.drawable.b51, R.drawable.b61, R.drawable.b71, R.drawable.b81, R.drawable.b91, R.drawable.b101, R.drawable.b111, R.drawable.b121, R.drawable.b131, R.drawable.b141, R.drawable.b151, R.drawable.b161, R.drawable.b171};
-    private final static int[] backgroundSrc2 = {R.drawable.b12, R.drawable.b22, R.drawable.b32, R.drawable.b42, R.drawable.b52, R.drawable.b62, R.drawable.b72, R.drawable.b82, R.drawable.b92, R.drawable.b102, R.drawable.b112, R.drawable.b122, R.drawable.b132, R.drawable.b142, R.drawable.b152, R.drawable.b162, R.drawable.b172};
+    private final static int[] backgroundSrc1 = {R.drawable.b11, R.drawable.b21, R.drawable.b31, R.drawable.b41};
+    private final static int[] backgroundSrc2 = {R.drawable.b12, R.drawable.b22, R.drawable.b32, R.drawable.b42};
 
     public static int[] getBackgroundSrc1() {
         return backgroundSrc1;
